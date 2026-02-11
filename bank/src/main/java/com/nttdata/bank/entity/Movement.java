0e26ad758d7f73a6deec8dffd15916a87fb67332
Sergio Hidalgo <barcecheo2319@gmail.com>
2026-02-11 11:27:09 -0500

    BE: Add relation between Movement and Account

--- bank/src/main/java/com/nttdata/bank/entity/Movement.java
+++ bank/src/main/java/com/nttdata/bank/entity/Movement.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import java.sql.Timestamp;
 import lombok.Getter;
 import lombok.Setter;
@@ -21,6 +23,10 @@ public class Movement {
   private MovementType movementType;
   private Double value;
   private Double balance;
+
+  @OneToOne
+  @JoinColumn(name = "account_id", referencedColumnName = "id")
+  private Account account;
 
   enum MovementType {
     DEPOSIT,
