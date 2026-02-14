7b1ebde05ded7c20f6786eee9a93934cc4c8e39e
Sergio Hidalgo <barcecheo2319@gmail.com>
2026-02-14 14:10:09 -0500

    BE: Add CORS

--- bank/src/main/java/com/nttdata/bank/controller/MovementController.java
+++ bank/src/main/java/com/nttdata/bank/controller/MovementController.java
@@ -5,12 +5,14 @@ import com.nttdata.bank.dto.MovementDto;
 import com.nttdata.bank.service.MovementService;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/bank/v1/movements")
 public class MovementController {
