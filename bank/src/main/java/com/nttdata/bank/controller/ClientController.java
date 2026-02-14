64e698c8f15709e6438fe2a552d9fc7fad532d51
Sergio Hidalgo <barcecheo2319@gmail.com>
2026-02-14 00:03:26 -0500

    BE: Add CORS

--- bank/src/main/java/com/nttdata/bank/controller/ClientController.java
+++ bank/src/main/java/com/nttdata/bank/controller/ClientController.java
@@ -6,6 +6,7 @@ import com.nttdata.bank.service.ClientService;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/bank/v1/clients")
 public class ClientController {
